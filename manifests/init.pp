0b0ab32b3323cc4aa09fa29ca8f1d3c949bd574c
Josh Russell <jrussell@hubspot.com>
2013-03-26 11:56:39 -0400

    Add parameters for package control

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,54 +9,59 @@
 # Usage:
 #
 class nodejs(
-  $dev_package = false,
-  $proxy       = ''
+  $node_pkg       = $nodejs::params::node_pkg,
+  $npm_pkg        = $nodejs::params::npm_pkg,
+  $configure_repo = true,
+  $dev_package    = false,
+  $proxy          = ''
 ) inherits nodejs::params {
 
-  case $::operatingsystem {
-    'Debian': {
-      include 'apt'
+  if $configure_repo {
+    case $::operatingsystem {
+      'Debian': {
+        include 'apt'
 
-      apt::source { 'sid':
-        location    => 'http://ftp.us.debian.org/debian/',
-        release     => 'sid',
-        repos       => 'main',
-        pin         => 100,
-        include_src => false,
-        before      => Anchor['nodejs::repo'],
+        apt::source { 'sid':
+          location    => 'http://ftp.us.debian.org/debian/',
+          release     => 'sid',
+          repos       => 'main',
+          pin         => 100,
+          include_src => false,
+          before      => Anchor['nodejs::repo'],
+        }
+
       }
 
-    }
+      'Ubuntu': {
+        include 'apt'
 
-    'Ubuntu': {
-      include 'apt'
-
-      # Only use PPA when necessary.
-      if $::lsbdistcodename != 'Precise'{
-        apt::ppa { 'ppa:chris-lea/node.js':
-          before => Anchor['nodejs::repo'],
+        # Only use PPA when necessary.
+        if $::lsbdistcodename != 'Precise'{
+          apt::ppa { 'ppa:chris-lea/node.js':
+            before => Anchor['nodejs::repo'],
+          }
         }
       }
-    }
 
-    'Fedora', 'RedHat', 'CentOS', 'OEL', 'OracleLinux', 'Amazon': {
-      package { 'nodejs-stable-release':
-        ensure => absent,
-        before => Yumrepo['nodejs-stable'],
+      'Fedora', 'RedHat', 'CentOS', 'OEL', 'OracleLinux', 'Amazon': {
+        package { 'nodejs-stable-release':
+          ensure => absent,
+          before => Yumrepo['nodejs-stable'],
+        }
+
+        yumrepo { 'nodejs-stable':
+          descr    => 'Stable releases of Node.js',
+          baseurl  => $nodejs::params::baseurl,
+          enabled  => 1,
+          gpgcheck => $nodejs::params::gpgcheck,
+          gpgkey   => 'http://patches.fedorapeople.org/oldnode/stable/RPM-GPG-KEY-tchol',
+          before   => Anchor['nodejs::repo'],
+        }
       }
 
-      yumrepo { 'nodejs-stable':
-        descr    => 'Stable releases of Node.js',
-        baseurl  => $nodejs::params::baseurl,
-        enabled  => 1,
-        gpgcheck => $nodejs::params::gpgcheck,
-        gpgkey   => 'http://patches.fedorapeople.org/oldnode/stable/RPM-GPG-KEY-tchol',
-        before   => Anchor['nodejs::repo'],
+      default: {
+        fail("Class nodejs does not support ${::operatingsystem}")
       }
-    }
-
-    default: {
-      fail("Class nodejs does not support ${::operatingsystem}")
     }
   }
 
@@ -64,15 +69,23 @@ class nodejs(
   anchor { 'nodejs::repo': }
 
   package { 'nodejs':
-    name    => $nodejs::params::node_pkg,
+    name    => $node_pkg,
     ensure  => present,
-    require => Anchor['nodejs::repo']
+    require => $configure_repo ? {
+      true    => Anchor['nodejs::repo'],
+      default => undef,
+    }
   }
 
-  package { 'npm':
-    name    => $nodejs::params::npm_pkg,
-    ensure  => present,
-    require => Anchor['nodejs::repo']
+  if $npm_pkg {
+    package { 'npm':
+      name    => $npm_pkg,
+      ensure  => present,
+      require => $configure_repo ? {
+        true    => Anchor['nodejs::repo'],
+        default => undef,
+      }
+    }
   }
 
   if $proxy {
